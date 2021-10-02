445ba039629f6634298a9878134a8490803e7b95
Gervais Codes <gervaiscodes@protonmail.com>
2021-10-02 15:47:32 +0200

    Blockchain validation

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -17,6 +17,23 @@ class Blockchain
     chain.last
   end
 
+  def block_at(index)
+    chain[index]
+  end
+
+  def add_block(block)
+    chain.push(block)
+  end
+
+  def valid?
+    (1...length).each do |position|
+      previous_block = block_at(position - 1)
+      current_block = block_at(position)
+      return false if current_block.previous_hash != previous_block.hash
+    end
+    true
+  end
+
   private
 
   def genesis_block
--- spec/blockchain_spec.rb
+++ spec/blockchain_spec.rb
@@ -3,13 +3,65 @@
 require 'blockchain'
 
 RSpec.describe Blockchain do
-  subject { Blockchain.new }
+  let(:blockchain) { Blockchain.new }
 
   describe 'blockchain initialization' do
     it 'creates genesis block' do
-      expect(subject.length).to eq(1)
-      expect(subject.last_block.index).to eq(0)
-      expect(subject.last_block.previous_hash).to eq('')
+      expect(blockchain.length).to eq(1)
+      expect(blockchain.last_block.index).to eq(0)
+      expect(blockchain.last_block.previous_hash).to eq('')
+    end
+  end
+
+  describe 'blockchain validation' do
+    context 'valid blockchain' do
+      before do
+        blockchain.add_block(
+          Block.new(
+            index: 1,
+            timestamp: 1_633_173_441,
+            previous_hash: '075c27741a3506846368fa6e5b3477f85b31ceee71a5716e2f12b40fa21d23aa',
+            data: 'Second block'
+          )
+        )
+        blockchain.add_block(
+          Block.new(
+            index: 2,
+            timestamp: 1_633_173_565,
+            previous_hash: '5b781b11ade2d9f65010e18aaf809d67a37757af5173aa6f8385de19cb5d71e6',
+            data: 'Third block'
+          )
+        )
+      end
+
+      it 'validates blockchain' do
+        expect(blockchain.valid?).to be true
+      end
+    end
+
+    context 'invalid blockchain' do
+      before do
+        blockchain.add_block(
+          Block.new(
+            index: 1,
+            timestamp: 1_633_173_441,
+            previous_hash: '075c27741a3506846368fa6e5b3477f85b31ceee71a5716e2f12b40fa21d23aa',
+            data: 'Second block MODIFIED'
+          )
+        )
+        blockchain.add_block(
+          Block.new(
+            index: 2,
+            timestamp: 1_633_173_565,
+            previous_hash: '5b781b11ade2d9f65010e18aaf809d67a37757af5173aa6f8385de19cb5d71e6',
+            data: 'Third block'
+          )
+        )
+      end
+
+      it 'invalidates blockchain' do
+        expect(blockchain.valid?).to be false
+      end
     end
   end
 end
