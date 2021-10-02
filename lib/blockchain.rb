88b1acabc698a0984d5e3350fd656947cf23507f
Gervais Codes <gervaiscodes@protonmail.com>
2021-10-03 01:11:23 +0200

    Sinatra wrapper

--- lib/blockchain.rb
+++ lib/blockchain.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'block'
+require './lib/block'
 
 class Blockchain
   DIFFICULTY = 4
--- lib/server.rb
+++ lib/server.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require_relative 'blockchain'
+require './lib/blockchain'
 
-blockchain = Blockchain.new
+class BlockchainApp < Sinatra::Base
+  attr_reader :blockchain
 
-get '/blocks' do
-  blockchain.blocks.map(&:to_h).to_json
-end
+  def initialize
+    super
+    @blockchain = Blockchain.new
+  end
 
-post '/mine' do
-  request.body.rewind
-  body = JSON.parse(request.body.read)
-  data = body['data']
-  next_block = blockchain.mine_block(data: data)
-  blockchain.append(next_block)
-  next_block.to_h.to_json
+  get '/blocks' do
+    blockchain.blocks.map(&:to_h).to_json
+  end
+
+  post '/mine' do
+    request.body.rewind
+    body = JSON.parse(request.body.read)
+    data = body['data']
+    next_block = blockchain.mine_block(data: data)
+    blockchain.append(next_block)
+    next_block.to_h.to_json
+  end
 end
