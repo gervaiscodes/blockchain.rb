b0505f8f5ea488d59192607b1e5bf2ab14859eeb
Gervais Codes <gervaiscodes@protonmail.com>
2021-10-14 16:40:51 +0200

    Specs : longest_chain_from_peers

--- lib/p2p.rb
+++ lib/p2p.rb
@@ -22,11 +22,11 @@ class P2P
   end
 
   def longest_chain_from_peers
-    peers.each_with_object([]) do |url, longest|
+    peers.inject([]) do |longest, url|
       res = Net::HTTP.get(URI("#{url}/blocks"))
       parsed = JSON.parse(res)
       chain = Blockchain.new(blocks: parsed.map { |block| Block.from_hash(block) })
-      return chain.blocks if chain.length > longest.length && chain.valid?
+      chain.length > longest.length && chain.valid? ? chain.blocks : longest
     end
   end
 end
--- spec/p2p_spec.rb
+++ spec/p2p_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'p2p'
+require 'blockchain'
 
 RSpec.describe P2P do
   let(:p2p) do
@@ -49,5 +50,48 @@ RSpec.describe P2P do
         thread.join
       end
     end
+  end
+
+  describe 'longest_chain_from_peers' do
+    let(:peers) do
+      [{
+        url: 'http://host:7777',
+        blocks: DummyBlockchainFactory.new(3).call.blocks.map(&:to_h).to_json
+      }, {
+        url: 'http://host:8888',
+        blocks: DummyBlockchainFactory.new(8).call.blocks.map(&:to_h).to_json
+      }, {
+        url: 'http://host:9999',
+        blocks: DummyBlockchainFactory.new(4).call.blocks.map(&:to_h).to_json
+      }]
+    end
+
+    before do
+      peers.each do |peer|
+        stub_request(:get, "#{peer[:url]}/blocks").to_return(body: peer[:blocks])
+        p2p.add_peer(peer[:url])
+      end
+    end
+
+    it 'gets the longest chain from peers' do
+      expect(p2p.longest_chain_from_peers.length).to eq(8)
+    end
+  end
+end
+
+class DummyBlockchainFactory
+  attr_reader :length, :blockchain
+
+  def initialize(length)
+    @length = length
+    @blockchain = Blockchain.new
+  end
+
+  def call
+    (1...length).each do |position|
+      block = blockchain.mine_block(data: position)
+      blockchain.append(block)
+    end
+    blockchain
   end
 end
